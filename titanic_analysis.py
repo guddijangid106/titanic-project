a88a3cff565e9a641e7a9de9913933baf13bea3b
guddijangid106 <guddijangid106@gmail.com>
2025-03-11 20:29:35 +0530

    Fixed

--- titanic_analysis.py
+++ titanic_analysis.py
@@ -1,4 +1,6 @@
 import pandas as pd  # Import pandas for data handling
+import seaborn as sns
+import matplotlib.pyplot as plt
 
 # Load the dataset
 df = pd.read_csv("titanic.csv")
@@ -8,6 +10,9 @@ print(df.head())
 
 # Show general info (columns, data types, missing values)
 print(df.info())
+
+sns.countplot(x="Survived", data=df)
+plt.show()
 
 # Show missing values count
 print(df.isnull().sum())
